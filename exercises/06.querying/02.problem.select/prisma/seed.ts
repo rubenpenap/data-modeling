147dc7c03fc8150d063e98c0aaae996b08f6daba
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-05-10 16:08:29 -0400

    refactor: add select option to user creation in seed script to optimize data retrieval

--- exercises/06.querying/02.problem.select/prisma/seed.ts
+++ exercises/06.querying/02.problem.select/prisma/seed.ts
@@ -110,8 +110,7 @@ async function seed() {
 	for (let index = 0; index < totalUsers; index++) {
 		await prisma.user
 			.create({
-				// 🐨 add a select to just get the ID so we're not pulling back the
-				// entire user object.
+				select: { id: true },
 				data: {
 					...createUser(),
 					image: { create: userImages[index % 10] },
@@ -176,8 +175,7 @@ async function seed() {
 	})
 
 	await prisma.user.create({
-		// 🐨 add a select to just get the ID so we're not pulling back the
-		// entire user object.
+		select: { id: true },
 		data: {
 			email: 'kody@kcd.dev',
 			username: 'kody',
